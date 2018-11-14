3777d0ded3fa831a2a88aee93f762af4e3ff5117
bdoffr <ms626547127@gmail.com>
2018-11-15 01:20:35 +0800

    详情页及模版渲染

--- books/views.py
+++ books/views.py
@@ -40,14 +40,23 @@ def index(request):
     # 使用模板
     return render(request, 'books/index.html', context)
 
-def detail(request,books_id):
+def detail(request, books_id):
+    '''显示商品的详情页面'''
+    # 获取商品的详情信息
     books = Books.objects.get_books_by_id(books_id=books_id)
+
     if books is None:
+        # 商品不存在，跳转到首页
         return redirect(reverse('books:index'))
-    books_li = Books.object.get_books_by_type(type_id=books.type_id,limit=2,sort='new')
+
+    # 新品推荐
+    books_li = Books.objects.get_books_by_type(type_id=books.type_id, limit=2, sort='new')
+    
+    # 当前商品类型
     type_title = BOOKS_TYPE[books.type_id]
-    context = {'books' : books,
-                'book_li' : books_li,
-                'type_title' : type_title
-                }
-    return render(request,'books/detail.html',context)
+    
+    # 定义上下文
+    context = {'books': books, 'books_li': books_li, 'type_title':type_title}
+
+    # 使用模板
+    return render(request, 'books/detail.html', context)
